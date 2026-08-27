bc73923c19ce2a0daca15df7c9b25f9a3146b8c2
Lua39683512 <1667124821@qq.com>
2026-08-27 14:47:03 +0800

    Add files via upload
    Signed-off-by: Lua39683512 <1667124821@qq.com>

---
+++ 下载.lua
@@ -0,0 +1,13 @@
+local url="https://raw.githubusercontent.com/Lua39683512/Lua/main/zhqf.lua"
+local res=gg.makeRequest(url)
+if not res then
+print("无法连接GitHub")end
+if res.code~=200 then
+gg.toast("下载失败\n状态码:"..res.code)end
+local save="/sdcard/zhqf.lua"
+local f=io.open(save,"w")
+if not f then
+print("无法创建文件")end
+f:write(res.content)
+f:close()
+print("下载成功"..save)
